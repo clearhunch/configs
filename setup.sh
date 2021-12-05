b33b6411888875dc162f4e653fe4e250e85dbea6
clearhunch <chrisedge44@gmail.com>
2021-12-05 16:09:06 -0500

    More changes'

--- setup.sh
+++ setup.sh
@@ -2,16 +2,21 @@ test_path () {
   which $1 > /dev/null
 }
 xcode-select --install
-
 test_path brew
 if [[ $? -ne 0 ]]
 then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
+brew install zsh
 brew install --cask visual-studio-code
 brew install --cask docker
 brew install nvm
-mv ./nvim ~/.config/nvim
 brew install --cask hyper
+brew install --cask dbeaver-community
+brew install --cask firefox
+brew install --cask dashlane
+brew install --cask spotify
+mv ./nvim ~/.config/nvim
 mv .hyper.js ~/
+
